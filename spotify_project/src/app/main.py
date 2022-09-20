5a6b69c2267e6fe73ac855ced59030c9a98ef803
carloJose <70476568+carloJose@users.noreply.github.com>
2022-09-20 01:02:53 -0300

    api rodando

---
+++ spotify_project/src/app/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def boot():
+    return {"message": "Bem-vindo a api que vai fazer as tuas melhores Playlists"}
